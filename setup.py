8ff7d894cb93c7d50e8795678dafdd3c8990193d
Jelle Bakker <jellebakker@jb-audio.com>
2022-07-01 15:04:04 +0200

    Remove "udev" linux library from setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if platform.system() == "Darwin":
     os.environ["LDFLAGS"] = "-framework cocoa -framework IOKit"
     libraries = ["ximu3"]
 elif platform.system() == "Linux":
-    libraries = ["ximu3", "udev" ]
+    libraries = ["ximu3"]
 else:
     libraries = ["ximu3", "ws2_32", "userenv", "setupapi", "advapi32"]
 
