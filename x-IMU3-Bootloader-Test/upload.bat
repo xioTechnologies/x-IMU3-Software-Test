193f2952d49701461f76edcaebfd4034badcc471
x-io Technologies <social@x-io.co.uk>
2022-09-20 22:08:05 +0100

    Add upload.bat for testing

---
+++ x-IMU3-Bootloader-Test/upload.bat
@@ -0,0 +1,9 @@
+@echo off
+
+..\cmake-build-debug\x-IMU3-Bootloader-Test\x-IMU3-Bootloader-Test.exe PIC32MZ2048EFG124 "C:\MyFiles\x-io\Products\x-IMU3\x-IMU3-Firmware\hex-archive\x-IMU3-Firmware-v1.0.0.hex" -b
+
+@REM ..\cmake-build-debug\x-IMU3-Bootloader-Test\x-IMU3-Bootloader-Test.exe PIC32MX470F512H "C:\MyFiles\x-io\Products\FARO IMU\FARO-IMU-Firmware\hex-archive\FARO-IMU-Firmware-v1.0.0.hex" -b
+
+@REM ..\cmake-build-debug\x-IMU3-Bootloader-Test\x-IMU3-Bootloader-Test.exe PIC32MX470F512L "C:\MyFiles\x-io\Products\NGIMU\NGIMU-Firmware\hex-archive\NGIMU-Firmware-v1.14.hex"
+
+pause
