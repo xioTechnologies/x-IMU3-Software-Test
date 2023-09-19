4e071c5d9c644be1332e2e354f2d98f6caecd7fc
x-io Technologies <social@x-io.co.uk>
2023-09-19 16:34:16 +0100

    Increase margin to 2%

--- x-IMU3-GUI/Source/OpenGL/Graph/AxesLimits.h
+++ x-IMU3-GUI/Source/OpenGL/Graph/AxesLimits.h
@@ -126,7 +126,7 @@ public:
 
             if (newY.min != std::numeric_limits<float>::max())
             {
-                const auto margin = juce::exactlyEqual(newY.min, newY.max) ? 1.0f : (0.01f * y.getRange()); // add 1% margin
+                const auto margin = juce::exactlyEqual(newY.min, newY.max) ? 1.0f : (0.02f * y.getRange()); // add 5% margin
                 y.min = newY.min - margin;
                 y.max = newY.max + margin;
             }
